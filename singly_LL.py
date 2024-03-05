ad7a27ab130f72563fa5a14ff326e72d4bb3ab07
Amit Deo <amitdeo2801@gmail.com>
2024-03-05 22:31:07 +0530

    update

--- singly_LL.py
+++ singly_LL.py
@@ -8,6 +8,9 @@ class SLL:
         self.start = start
     def is_empty(self):
         return self.start == None
+    def insert_at_start(self, data):
+        n = Node(data, self.start)
+        self.start - n 
     def insert_at_last(self, data):
         n = Node(data)
         if not self.is_empty():
